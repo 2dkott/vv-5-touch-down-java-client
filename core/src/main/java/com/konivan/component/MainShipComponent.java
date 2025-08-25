f434365f266852433e680a79e04e0400deaca40d
Konstantin Ivanov <kot2d@outlook.com>
2025-08-25 23:53:07 +0300

    added package with component

---
+++ core/src/main/java/com/konivan/component/MainShipComponent.java
@@ -0,0 +1,10 @@
+package com.konivan.component;
+
+import com.artemis.Component;
+import com.artemis.PooledComponent;
+
+public class MainShipComponent extends PooledComponent {
+	@Override
+	public void reset() {
+	}
+}
