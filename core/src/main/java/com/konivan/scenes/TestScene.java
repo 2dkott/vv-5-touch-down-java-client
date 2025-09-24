2199e0ebf46262c6ea349dc67a3b13bfd3b4208d
Konstantin Ivanov <kot2d@outlook.com>
2025-09-24 14:11:28 +0300

    updated graphics

--- core/src/main/java/com/konivan/scenes/TestScene.java
+++ core/src/main/java/com/konivan/scenes/TestScene.java
@@ -5,13 +5,15 @@ import com.konivan.game_objects.MainShipObject;
 
 import games.rednblack.editor.renderer.utils.ComponentRetriever;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class TestScene extends BaseScene{
 
     private final GameObject mainShip;
     public TestScene() {
         super("TestScene");
 
-        this.mainShip = findInScene(new MainShipObject());
+        this.mainShip = findInScene(MainShipObject.class);
         mainShip.getComponents().forEach(ComponentRetriever::addMapper);
 
         gameService.getRender().getCameraSystem().setFocus(this.mainShip.getEntity());
