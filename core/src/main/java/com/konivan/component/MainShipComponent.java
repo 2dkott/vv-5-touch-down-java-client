c076b5aec96818f6a47b8fd9fbc2830ed4222700
Konstantin Ivanov <kot2d@outlook.com>
2025-09-24 14:12:54 +0300

    remove redundant import

--- core/src/main/java/com/konivan/component/MainShipComponent.java
+++ core/src/main/java/com/konivan/component/MainShipComponent.java
@@ -1,9 +1,9 @@
 package com.konivan.component;
 
-import com.artemis.Component;
 import com.artemis.PooledComponent;
 
 public class MainShipComponent extends PooledComponent {
+
 	@Override
 	public void reset() {
 	}
