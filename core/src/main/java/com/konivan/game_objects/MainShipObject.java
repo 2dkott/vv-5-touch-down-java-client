a0fa364ef7ef08d70abf54d174858754e9905865
Konstantin Ivanov <kot2d@outlook.com>
2025-11-27 14:04:03 +0300

    added ShipMovement as instance

--- core/src/main/java/com/konivan/game_objects/MainShipObject.java
+++ core/src/main/java/com/konivan/game_objects/MainShipObject.java
@@ -3,20 +3,23 @@ package com.konivan.game_objects;
 import com.konivan.component.MainShipComponent;
 import com.konivan.scripts.ShipMovement;
 import games.rednblack.editor.renderer.utils.ItemWrapper;
+import lombok.Getter;
 
 public class MainShipObject extends GameObject {
 
-    @Override
-    public void setup(ItemWrapper characterItem) {
-        baseSetup(characterItem);
-        getComponents().add(MainShipComponent.class);
-        getCharacterItem().addScript(ShipMovement.class);
-    }
+	@Getter
+	private final ShipMovement shipMovement = new ShipMovement();
 
-    @Override
-    public String getName() {
-        return "main-ship-object";
-    }
+	@Override
+	public void setup(ItemWrapper characterItem) {
+		baseSetup(characterItem);
+		getComponents().add(MainShipComponent.class);
+		getCharacterItem().addScript(shipMovement);
+	}
 
+	@Override
+	public String getName() {
+		return "main-ship-object";
+	}
 
 }
