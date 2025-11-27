bf352c3d1b4826a4e2d775db19ce1e45471e1d5b
Konstantin Ivanov <kot2d@outlook.com>
2025-11-27 14:06:46 +0300

    added brakeForce

--- core/src/main/java/com/konivan/scripts/ShipMovement.java
+++ core/src/main/java/com/konivan/scripts/ShipMovement.java
@@ -1,30 +1,30 @@
 package com.konivan.scripts;
 
 import com.artemis.ComponentMapper;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import games.rednblack.editor.renderer.components.physics.PhysicsBodyComponent;
 import games.rednblack.editor.renderer.scripts.BasicScript;
+import lombok.Setter;
 
 public class ShipMovement extends BasicScript {
 
-	private PhysicsBodyComponent physicsBodyComponent;
 	protected ComponentMapper<PhysicsBodyComponent> physicMapper;
+
+	@Setter
+	private float brakeForce = 0f;
+	Body body;
 
 	@Override
 	public void init(int entity) {
 		super.init(entity);
-		physicsBodyComponent = physicMapper.get(entity);
+		body = physicMapper.get(entity).body;
 
 	}
 
 	@Override
 	public void act(float delta) {
 
-		Body body = physicsBodyComponent.body;
-		Vector2 velocity = body.getLinearVelocity();
-		Vector2 dampingForce = velocity.scl(-1000.1f);
-		body.applyForceToCenter(dampingForce, true);
+		body.applyForceToCenter(body.getLinearVelocity().scl(brakeForce * -10), true);
 	}
 
 	@Override
